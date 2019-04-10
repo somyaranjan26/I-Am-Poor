305d0bf9f88814ea324a9c225e89583261367b95
Somyaranjan <somyaranjanmac1@gmail.com>
2019-04-10 15:19:13 +0530

    adding main icons and swift files

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Somyaranjan Rout on 04/04/19.
+//  Copyright © 2019 Somyaranjan Rout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
